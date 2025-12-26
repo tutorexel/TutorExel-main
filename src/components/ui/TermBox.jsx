c35e95d6ca36e191b2318bdf2bc46693a603d8c3
mukii23 <mukii2307@gmail.com>
2025-12-26 15:51:38 +0530

    Content update and few changes UI

--- src/components/ui/TermBox.jsx
+++ src/components/ui/TermBox.jsx
@@ -11,7 +11,7 @@ const TermBox = ({ year, subject, termNumber, title, topics, bgColor }) => {
             <div className="term-content-wrapper">
                 <Row className="term-header gx-0">
                     <Col xs={1} className="th">S.No</Col>
-                    <Col xs={4} className="th">Topic Name</Col>
+                    <Col xs={4} className="th">Topic</Col>
                     <Col xs={7} className="th">What We Cover</Col>
                 </Row>
 
